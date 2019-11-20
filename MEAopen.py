f504dd543cf43b7fb466405b39307d74fd18281f
enzomarra <enzomarra@gmail.com>
2019-11-20 16:36:56 +0000

    Update MEAopen.py

--- MEAopen.py
+++ MEAopen.py
@@ -87,9 +87,9 @@ def do_analysis(rec):
     epsp2start=int(20*float(input("Start time slope 2: ")))
     epsp1slopes=[]
     epsp2slopes=[]
-    for i in range(1,rec1.sweep_n+1):
-        epsp1slopes.append(fepsp_slope(rec1.get_chan(chan,i)[epsp1start:epsp1start+80].values))
-        epsp2slopes.append(fepsp_slope(rec1.get_chan(chan,i)[epsp2start:epsp2start+80].values))
+    for i in range(1,rec.sweep_n+1):
+        epsp1slopes.append(fepsp_slope(rec.get_chan(chan,i)[epsp1start:epsp1start+80].values))
+        epsp2slopes.append(fepsp_slope(rec.get_chan(chan,i)[epsp2start:epsp2start+80].values))
     print(epsp1slopes)
     print(epsp2slopes)
     
